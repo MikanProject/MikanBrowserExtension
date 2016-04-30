c7f2563d13134e1df99c0e93fd63c7e6d65a5752
arition <aritionkb@gmail.com>
2016-04-29 18:58:39 -0700

    add jquery

--- Mikan/src/bg/background.js
+++ Mikan/src/bg/background.js
@@ -1,10 +1,19 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-var settings = new Store("settings", {
-    "sample_setting": "This is how you use Store.js to remember values"
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+
+chrome.tabs.create({
+    active: false,
+    url: 'http://stackoveflow.com/robots.txt'
+}, function(tab) {
+    chrome.tabs.executeScript(tab.id, {
+        code: 'localStorage.setItem("key", "value");'
+    }, function() {
+        chrome.tabs.remove(tab.id);
+    });
 });
-
-
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
