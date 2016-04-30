506917fd53d65549ca27f58e2c54e763390df83f
arition <aritionkb@gmail.com>
2016-04-29 22:29:43 -0700

    cleanup

--- Mikan/src/bg/background.js
+++ Mikan/src/bg/background.js
@@ -4,20 +4,35 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-chrome.tabs.create({
-    active: false,
-    url: 'http://stackoveflow.com/robots.txt'
-}, function(tab) {
-    chrome.tabs.executeScript(tab.id, {
-        code: 'localStorage.setItem("key", "value");'
-    }, function() {
-        chrome.tabs.remove(tab.id);
+chrome.storage.local.get("hash",
+    function(data) {
+        if (data == null) {
+            chrome.tabs.create(
+                {
+                    active: false,
+                    url: "http://mikanani.me/Account/ApiLogin"
+                },
+                function(tab) {
+                    chrome.tabs.executeScript(tab.id,
+                        {
+                            code: 'chrome.runtime.sendMessage({type: "hash", hash: localstorage});'
+                            
+                        },
+                        function() {
+                            chrome.tabs.remove(tab.id);
+                        });
+                });
+        } else {
+            $.ajaxSetup({
+                headers: {
+                    "Authorization": "MikanHash " + data
+                }
+            });
+        }
     });
-});
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+    function (request, sender, sendResponse) {
+
+    });
