12cd27c00419c9cedf311548a08530f50d07824c
arition <aritionkb@gmail.com>
2016-06-29 09:46:42 +0800

    add build config for firefox

---
+++ Mikan/webpack.release.firefox.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+const buildPath = path.resolve(__dirname, 'dist/src');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+const config = {
+  entry: [path.join(__dirname, '/src/index.jsx')],
+  resolve: {
+    //When require, do not have to add these extensions to file's name
+    extensions: ["", ".js", ".jsx"],
+    //node_modules: ["web_modules", "node_modules"]  (Default Settings)
+  },
+  //output config
+  output: {
+    path: buildPath,    //Path of output file
+    filename: '_index.js',  //Name of output file
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /(\.js$)|(\.jsx$)/,  //All .js files
+        loaders: ['babel-loader'],
+        exclude: [nodeModulesPath],
+      },
+    ],
+  },
+  //Eslint config
+  eslint: {
+    configFile: '.eslintrc', //Rules for eslint
+  },
+};
+
+module.exports = config;
